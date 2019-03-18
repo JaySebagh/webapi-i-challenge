0da4b2eae45955ebdc345d06198d30581f23fae9
JaySebagh <jaysebagh@gmail.com>
2019-03-18 13:04:02 -0700

    setup server and got hello world displaying

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// implement your API here
+// implement your API here]
+// import express 
+const express = require('express');
+const server = express(); // creates new http server
+// routes === endpoints
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+server.listen(4000, () => {
+    console.log('\n*** Running on port 4k ***\n')
+})

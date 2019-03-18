040c3166be5059993153b622aad4eead2fb87195
JaySebagh <jaysebagh@gmail.com>
2019-03-18 13:14:33 -0700

    got the array of objects to display on screen under path /api/users

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // implement your API here]
 // import express 
 const express = require('express');
+
+const db = require('./data/db.js')
+
 const server = express(); // creates new http server
 // routes === endpoints
 server.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+// The R in CRUD
+server.get('/api/users', (req, res) => {
+    db
+    .find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(err.code).json({ success: false, message: err.message })
+    })
+})
+
 
 server.listen(4000, () => {
     console.log('\n*** Running on port 4k ***\n')

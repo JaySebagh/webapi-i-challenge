255ab6eecb73aab347f740396371c39f2ea921d0
JaySebagh <jaysebagh@gmail.com>
2019-03-18 13:56:02 -0700

    can add

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const express = require('express');
 const db = require('./data/db.js')
 
 const server = express(); // creates new http server
+
+// middleware
+server.use(express.json());
+
 // routes === endpoints
 server.get('/', (req, res) => {
     res.send('Hello World!')
@@ -22,6 +26,19 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+// The C in CRUD
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+
+    db
+    .insert(user)
+    .then(users => {
+        res.status(201).json({ success: true, users });
+    })
+    .catch(({ code, message }) => {
+        res.status(code).json({ success: false, message });
+    });
+});
 
 server.listen(4000, () => {
     console.log('\n*** Running on port 4k ***\n')
